fd4da0c2b980c38b49df42b125804e7e3babf4ec
AshuMishraG <aashutoshm77@gmail.com>
2025-03-09 05:12:30 +0530

    Initial Commit

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -5,7 +5,15 @@
 //
 // Notes:
 // * Use a function to display your first name
+fn first_name() {
+    println!("Ashutosh");
+}
 // * Use a function to display your last name
+fn last_name() {
+    println!("Mishra");
+}
 // * Use the println macro to display messages to the terminal
-
-fn main() {}
+fn main() {
+    first_name();
+    last_name();
+}
