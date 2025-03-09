a6eaaf5d8767742231cd65429366041e61daa2c8
AshuMishraG <aashutoshm77@gmail.com>
2025-03-10 01:53:21 +0530

    if else statements

--- activities/src/bin/main.rs
+++ activities/src/bin/main.rs
@@ -1,16 +1,31 @@
-fn sub(a: i32, b: i32) -> i32 {
-    a - b
-}
-fn main() {
-    let sum = 2 + 2;
-    let value = 10 - 5;
-    let division = 10 / 2;
-    let multi = 5 * 5; // Ensure the variable name matches
-    let five = sub(8, 3);
+// Numeric Operators
+// fn sub(a: i32, b: i32) -> i32 {
+//     a - b
+// }
+// fn main() {
+//     let sum = 2 + 2;
+//     let value = 10 - 5;
+//     let division = 10 / 2;
+//     let multi = 5 * 5; // Ensure the variable name matches
+//     let five = sub(8, 3);
+//     let rem1 = 6 % 3;
+//     let rem2 = 6 % 4;
+//
+//     println!("Sum: {}", sum);
+//     println!("Value: {}", value);
+//     println!("Division: {}", division);
+//     println!("Multiplication: {}", multi); // Use 'multi' instead of 'mult'
+//     println!("Result of sub(8, 3): {}", five);
+//     println!("rem 1: {}", rem1);
+//     println!("rem 2: {}", rem2);
+//}
 
-    println!("Sum: {}", sum);
-    println!("Value: {}", value);
-    println!("Division: {}", division);
-    println!("Multiplication: {}", multi); // Use 'multi' instead of 'mult'
-    println!("Result of sub(8, 3): {}", five);
+// Demo.rs exceptions if statements
+fn main() {
+    let age = 15;
+    if age >= 21 {
+        println!("ok to purchase");
+    } else {
+        println!("cannot purchase");
+    }
 }
