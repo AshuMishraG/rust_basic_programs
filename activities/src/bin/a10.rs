819a67aa0c6f3b59a64502f3c5d0e8b1bfd22a28
AshuMishraG <aashutoshm77@gmail.com>
2025-03-16 19:05:15 +0530

    Day-10

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -5,11 +5,21 @@
 // * Print "its small" if a variable is <= 100
 //
 // Notes:
-// * Use a boolean variable set to the result of
-//   an if..else expression to store whether the value
-//   is > 100 or <= 100
 // * Use a function to print the messages
-// * Use a match expression to determine which message
-//   to print
+fn print_message(gt_100: bool) {
+    // * Use a match expression to determine which message
+    //   to print
+    match gt_100 {
+        true => println!("it's big"),
+        false => println!("it's small"),
+    }
+}
 
-fn main() {}
+fn main() {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    //   is > 100 or <= 100
+    let value = 100;
+    let is_gt_100 = value > 100;
+    print_message(is_gt_100);
+}
