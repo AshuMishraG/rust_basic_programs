e9269a09df5be09b5bba92b64318e01147419b66
AshuMishraG <aashutoshm77@gmail.com>
2025-03-27 17:36:34 +0530

    Result

--- activities/src/bin/main.rs
+++ activities/src/bin/main.rs
@@ -326,10 +326,44 @@
 // fn main() { }
 
 // Standard Library
-fn main() {
-    let numbers = vec![1, 2, 3];
-    match numbers.is_empty() {
-        true => println!("no numbers"),
-        false => println!("has numbers"),
+// fn main() {
+//     let numbers = vec![1, 2, 3];
+//     match numbers.is_empty() {
+//         true => println!("no numbers"),
+//         false => println!("has numbers"),
+//     }
+// }
+
+// Result
+#[derive(Debug)]
+enum MenuChoice {
+    MainMenu,
+    Start,
+    Quit,
+}
+fn get_choice(input: &str) -> Result<MenuChoice, String> {
+    match input {
+        "mainMenu" => Ok(MenuChoice::MainMenu),
+        "start" => Ok(MenuChoice::Start),
+        "quit" => Ok(MenuChoice::Quit),
+        _ => Err("menu choice not found".to_owned()),
     }
+}
+fn print_choice(choice: &MenuChoice) {
+    println!("choice = {:?}", choice);
+}
+fn pick_choice(input: &str) -> Result<(), String> {
+    let choice: MenuChoice = get_choice(input)?;
+    print_choice(&choice);
+    Ok(())
+}
+fn main() {
+    let choice = pick_choice("end");
+    println!("choice value = {:?}", choice);
+    // let choice: Result<MenuChoice, _> = get_choice("leave");
+    // // println!("choice = {:?}", choice);
+    // match choice {
+    //     Ok(inner_choice) => print_choice(&inner_choice),
+    //     Err(e) => println!("error = {:?}", e),
+    // }
 }
